3699d410989b8e97cbf8848aba2f65fd53d129e5
Hasbi Rizaldi <hasbirizaldi5@gmail.com>
2026-02-08 20:22:32 +0700

    oke

--- src/api/data.js
+++ src/api/data.js
@@ -75,7 +75,7 @@ import dyah from "../assets/direksi/dyah.png";
 import niken from "../assets/direksi/niken.png";
 import gallery1 from "../assets/gallery/1.jpg";
 import gallery2 from "../assets/gallery/2.jpg";
-import gallery3 from "../assets/gallery/3.jpg";
+import gallery3 from "../assets/gallery/3.JPG";
 import gallery4 from "../assets/gallery/4.jpg";
 import gallery5 from "../assets/gallery/5.jpg";
 import gallery6 from "../assets/gallery/6.jpg";
